5e30424f888faadf8efdc53c4096a0404ddfd012
Utashi <utashi85@gmail.com>
2021-01-02 23:41:06 +0900

    function

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
+# 関数
+def increment(x):
+    return x + 1
+
+# 初期化
+i = 0
 # loop や if 文のブロックは indent で表現
-for i in range(100):
+while i < 100:
     print(i, 'Hello, World!!')
+    i = increment(i)
 print('End')
 # indent が異なる End は最後に一回だけ表示される
